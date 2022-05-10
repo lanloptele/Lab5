55143d3a8f5bb53e6da457708005a6d015deba19
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-05-10 13:11:53 +0000

    5.1 w ćw2

--- script.js
+++ script.js
@@ -15,11 +15,44 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO implement it
-  })
+    answer.innerHTML="Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(function(data){
+      appendData(data);
+    })
+    function appendData(data){
+
+      answer.innerHTML="";
+      for(var i=0;i<data.lenght;i++){
+        var div=document.createElement("div");
+        div.className="contener";
+        div.innerHTML='<strong>UserID:<a>'
+        +data[i].userld+'</a></strong><strong>id:<a>'
+        +data[i].id+'</a></strong><br><strong>title:</strong><a>'
+        +data[i].title+'</a><br><strong>body:</strong><a>'
+        +data[i].body+'</a><br><br>';
+        answer.appendChild(div);
+      }
+    }
+    })
+  
 
   cw2.addEventListener("click", function () {
-    //TODO implement it
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+      .then(array => {
+        answer.innerHTML="";
+        array.forEach(post=>answer.innerHTML +=
+          `
+       <div>
+      <ul>
+       <li>User: ${post.userId}</br> ID:${post.id}</br> title:${post.title}</br> body:${post.body}</br><li/>
+</ul>
+</div>
+`
+                      );
+      })
   })
 
   cw3.addEventListener("click", function () {
